b830b6b2d6352049f3d8cab21561e9092e948be8
Trent M. Wyatt <trent@trentwyatt.com>
2023-06-08 16:01:27 -0500

    Update CompileTime.ino

--- examples/CompileTime/CompileTime.ino
+++ examples/CompileTime/CompileTime.ino
@@ -3,7 +3,7 @@
  * 
  * example Arduino IDE sketch for the CompileTime library
  * 
- * version 1.0 written June 2023 - Trent m. Wyatt
+ * version 1.0 written June 2023 - Trent M. Wyatt
  * 
  */
 #include "CompileTime.h"
